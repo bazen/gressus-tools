f363e885f0f18f945bbe66fdb85e31cc2b86e2b5
bugspencer <felix.krueger@gressus.de>
2016-06-30 16:12:52 +0200

    Explode

---
+++ Classes/Converter/Explode.php
@@ -0,0 +1,40 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2016
+ *  All rights reserved
+ *
+ *  GRESSUS
+ *
+ * @category Gressus
+ * @package Gressus_Tools
+ ***************************************************************/
+namespace Gressus\Tools\Converter;
+use \Gressus\Tools\DataMapperService;
+/**
+ * Implode Converter
+ *
+ * @category Gressus
+ * @package Gressus_Tools
+ * @author Felix Krüger <f3l1x@gressus.de>
+ */
+class Explode extends AbstractConverter {
+
+	/**
+	 * Split String
+	 * @var string
+	 */
+	protected $options = ' , ';
+	/**
+	 * Implode Value
+	 * @param array $input
+	 * @param \Gressus\Tools\DataMapperService $dataMapper
+     * @param string $fieldName
+	 * @return string
+	 */
+	public function convert($input,DataMapperService $dataMapper,$fieldName){
+		return explode($this->options,$input);
+	}
+
+}
---
+++ Classes/Converter/LastOfArray.php
@@ -0,0 +1,44 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2016
+ *  All rights reserved
+ *
+ *  GRESSUS
+ *
+ * @category Gressus
+ * @package Gressus_Tools
+ ***************************************************************/
+namespace Gressus\Tools\Converter;
+use \Gressus\Tools\DataMapperService;
+/**
+ * Implode Converter
+ *
+ * @category Gressus
+ * @package Gressus_Tools
+ * @author Felix Krüger <f3l1x@gressus.de>
+ */
+class LastOfArray extends AbstractConverter {
+
+	/**
+	 * Split String
+	 * @var string
+	 */
+	protected $options = ' , ';
+	/**
+	 * Implode Value
+	 * @param array $input
+	 * @param \Gressus\Tools\DataMapperService $dataMapper
+     * @param string $fieldName
+	 * @return string
+	 */
+	public function convert($input,DataMapperService $dataMapper,$fieldName){
+	 	if(is_array($input)){ 
+			 return array_shift($input);
+		 }
+		//Fallback
+		return $input;
+	}
+
+}
